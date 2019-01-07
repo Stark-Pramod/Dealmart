e9f9fe260f7ede995660250768d898b1081bbd16
Pramod-Jaiswal <pramodjaiswal517@gmail.com>
2019-01-07 14:17:32 +0530

    seller and buyer merged into acounts

--- dealmart/dealmart/settings.py
+++ dealmart/dealmart/settings.py
@@ -43,10 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'buyer',
+    'accounts',
     'phonenumber_field',
     'rest_framework_jwt',
-    'seller',
     ]
 
 MIDDLEWARE = [
