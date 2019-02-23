447d5a6092640823ec9cdb234738b561af0e3b4f
Pramod-Jaiswal <pramodjaiswal517@gmail.com>
2019-02-23 23:52:30 +0530

    modified return data

--- dealmart/dealmart/settings.py
+++ dealmart/dealmart/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '62(nf21nr6d95y*%xfkr62sakn8o8&!@y&xk_@)6mmf-wn90&%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['5678ec1a.ngrok.io', '127.0.0.1']
+ALLOWED_HOSTS = ['ce945b6e.ngrok.io', '127.0.0.1']
 
 # Application definition
 
